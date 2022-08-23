0aa1dadb1501dad8a755bbdeeb83eae797e77502
SpLiYvIiI <32437231+SpLiYvIiI@users.noreply.github.com>
2022-08-23 21:31:22 +0400

    some message

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int parseFirst = Integer.parseInt(a);
+        int parseSecond = Integer.parseInt(b);
+        int first = 5 * (parseFirst * parseFirst) + 3;
+        int second = 5 * (parseSecond * parseSecond) + 3;
+
+        return first == second ? parseFirst - parseSecond : first - second;
     }
 }
